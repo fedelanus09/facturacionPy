33dce9c7f7a08a33013c81b475626ccb6486e550
Federico Moreno <fedelanus09@gmail.com>
2017-12-11 10:33:40 -0300

    Diseño de Menu Principal

--- mainMenu.py
+++ mainMenu.py
@@ -1,7 +1,9 @@
 def mainMenu():
+	from os import system
+	system('clear')
 	
-	print("     Menu Principal")
-	print("     ==== =========")
+	print("Menu Principal")
+	print("==== =========")
 	print()
 	print(" 1) Ingreso de mercaderia")
 	print(" 2) Lista de productos")
