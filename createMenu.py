fe96066ae045bcbe9a14e125424811e167c0e3cd
Federico Moreno <fedelanus09@gmail.com>
2017-12-11 10:32:43 -0300

    Menu de Ingreso de Productos

--- createMenu.py
+++ createMenu.py
@@ -1,42 +1,49 @@
-def createMenu():
-	system("clear")
+from os import system
+from time import time
+
+def registerMenu():
+	system('clear')
 	
-	print("   Registro de mercaderia")
-	print("   ======== == ==========")
+	print("Registro de mercaderia")
+	print("======== == ==========")
 	print()
 	print(" 1) Ingresar nuevo producto")
 	print(" 2) Volver al menu anterior")
 	print()
 
-def create(createMenu, newProduct):
+def create():
 	while True:
-		createMenu()
-	
+		registerMenu()
+		
 		createOption = input("Seleccione una opcion: ")
 	
 		if createOption == "1":
 			newProduct()
+			print()
+			input("Presione ENTER para volver al menu...")
 		elif createOption == "2":
-			break
+			menu()
 		else:
 			print()
 			print("ERROR! OPCION INCORRECTA")
 			sleep(1)
+		
 
 def newProduct():
-	system("clear")
+	system('clear')
 	
-	inventary = []
-	
-	for i in range(3):
+	inventory = []
+
+	for i in range(5):
 		products = {}
 		
+		system('clear')
+		print("INGRESO DE NUEVO PRODUCTO")
+		print()
 		products["quantity"] = int(input("Cantidad: "))
-		products["itemName"] = input("Nombre del producto: ")
-		products["priceItem"] = int(input("Precio unit. $: "))
+		products["product"] = input("Producto: ")
+		products["price"] = int(input("Precio: "))
 		
-		inventary.append(products)
-		
-	print()
-	print("Registro de producto exitosa...")
-	sleep(1)
+		inventory.append(products)
+
+	saveData()
